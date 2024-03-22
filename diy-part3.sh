30e55bdc690216080688d99fde68215c6a05ccf0
stcwl <96298241+stcwl@users.noreply.github.com>
2024-03-22 10:51:54 +0800

    Update diy-part3.sh

--- diy-part3.sh
+++ diy-part3.sh
@@ -21,4 +21,4 @@
 # https://github.com/kenzok8/small-package
 
 sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-ed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
